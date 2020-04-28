ad9b9eb588835029e767579f453f6bfff7f8aaa3
Anton Liubimov <ant.liubimov@gmail.com>
2020-04-28 17:28:40 +0500

    finish

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const htmlValidator = require('gulp-w3c-html-validator');
+const htmlmin = require('gulp-htmlmin');
 const posthtml = require('gulp-posthtml');
 const include = require('posthtml-include');
 
@@ -10,5 +11,6 @@ module.exports = function html() {
     .pipe(plumber())
     .pipe(posthtml([include()]))
     .pipe(htmlValidator())
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('build'));
 };
